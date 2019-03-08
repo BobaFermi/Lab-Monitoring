7f2b2f7a09ff3bc1010dacbb4e01e0be8d46a77d
Craig Colquhoun <48094998+BobaFermi@users.noreply.github.com>
2019-03-08 02:14:25 +0000

    Update TempACExpTable.ino

--- TempACExpTable.ino
+++ TempACExpTable.ino
@@ -1,32 +1,13 @@
 #include <Time.h>
 #include <TimeLib.h>
-
-/*****************************************************************************
-* Read temperature from Dallas sensors
-* +send to network server with udp
-* 
-* use uno and network shield
-*
-* Elmar Haller, Glasgow 2015
-*
-******************************************************************************/
-
-
 #include <SPI.h>                  // needed for Arduino versions later than 0018
 #include <Ethernet.h>
 #include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
 #include <Time.h> 
-//#include <Timezone.h>
-
-
-//United Kingdom (London, Belfast)
-//TimeChangeRule rBST = {"BST", Last, Sun, Mar, 1, 60};        //British Summer Time
-//TimeChangeRule rGMT = {"GMT", Last, Sun, Oct, 2, 0};         //Standard Time
-//Timezone UK(rBST, rGMT);
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+//instantiate OneWire objects
 OneWire oneWire1(2);
 OneWire oneWire2(3);
 OneWire oneWire3(8);
@@ -34,6 +15,7 @@ OneWire oneWire4(5);
 OneWire oneWire5(6);
 OneWire oneWire6(7);
 
+//instantiate temperature sensors using the OneWire objects
 DallasTemperature sensor1(&oneWire1);
 DallasTemperature sensor2(&oneWire2);
 DallasTemperature sensor3(&oneWire3);
@@ -41,66 +23,54 @@ DallasTemperature sensor4(&oneWire4);
 DallasTemperature sensor5(&oneWire5);
 DallasTemperature sensor6(&oneWire6);
 
-/************************************************************
-// network stuff
-/***********************************************************/
+ 
 
-// NTP Servers:
-IPAddress timeServer(130, 159, 248, 123);  // strathclyde
-const int timeZone = 0;  // Central European Time
+IPAddress timeServer(130, 159, 248, 123);  //strathclyde time server
+const int timeZone = 0;  //Central European Time
 
-// local MAC address, fake
-byte mac[] = { 0xAA, 0xDE, 0xBE, 0xCF, 0xFE, 0xDD };
-// local ip
-IPAddress localIPNum(172, 16, 1, 21);
+byte mac[] = { 0xAA, 0xDE, 0xBE, 0xCF, 0xFE, 0xDD };    //local MAC address, fake
+IPAddress localIPNum(172, 16, 1, 21);                   //local ip
 byte gatewayIPNum[] = { 172, 16, 0, 1 }; 
 byte dnsIPNum[]  = { 8, 8, 8, 8 }; 
 byte maskIPNum[] = { 255, 255, 0, 0 }; 
 
 
-// data collection server
+//IP and port for data collection server
 IPAddress outIP(172,16,1,10);
 unsigned int outPort = 8010;
 
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
+EthernetUDP Udp;    //instantiate UDP object
 
 /*****************************************************************************
 / general stuff
 ******************************************************************************/
-const int delayTime = 20000;  // in milliseconds
-
-
-/* send with netcat in linux: echo -n "@YUN#3335.4e19#234.11" | netcat -c -w1 -u 192.168.100.1 8010 */
-//String s1   = "echo -n \"@";
-//String s2   = "\" | netcat -c -w1 -u 172.16.0.1 8010 ";
-
+const int delayTime = 20000;  //in milliseconds
 
 
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(9600); //9600 baud rate
   delay(250);
-  Serial.println("Get NTP timer.");  
+  Serial.println("Get NTP timer.");   //debug
   
-  // start the Ethernet and UDP:
-  Ethernet.begin(mac, localIPNum, dnsIPNum, gatewayIPNum, maskIPNum );
-  Udp.begin(8888);
+  Ethernet.begin(mac, localIPNum, dnsIPNum, gatewayIPNum, maskIPNum );      //start ethernet with defined network details
+  Udp.begin(8888);                                                          //start UDP server on port 8888
 
-  Serial.print("Local IP number: ");  
+  //debug
+  Serial.print("Local IP number: ");    
   Serial.println(Ethernet.localIP() );
   Serial.println("Waiting time sync.");
   setSyncProvider(getNtpTime);  
   setSyncInterval(86400);  
 
-  while (timeStatus() == timeNotSet) {
-    Serial.println("Waiting time sync.");    
+  while (timeStatus() == timeNotSet) {      //until we get a time
+    Serial.println("Waiting time sync.");    //print every 3 seconds for debug
     delay(3000);
   }  
 
 
-  // Start up the temperature library
-  sensor1.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
+  //initialise temperature sensors
+  sensor1.begin();
   sensor2.begin();
   sensor3.begin();
   sensor4.begin();
@@ -113,29 +83,26 @@ void setup() {
 void getTempValues(void)
 {
   int   i;
-  float T1, T2, T3, T4, T5, T6;
+  float T1, T2, T3, T4, T5, T6;     //variables for temperature values
   
-  String nowStr;
-  String str0,str1m;  
+  String nowStr;                    //string for UNIX time
+  String str0,str1m;                //the number of measurements makes a string longer than the buffer will allow, need 2 strings
   
-  // buffer to send string with UDP
-  char UDPBuffer[512];          
+  char UDPBuffer[512];              //buffer to send string with UDP
+  
+  nowStr = String( now() );         //get time now
 
+  Serial.print("Requesting temperatures...");   //debug
   
-  // get time now
-  nowStr = String( now() );
-    
-  //******************************************************
-  // get temperature values
-  //******************************************************
-  Serial.print("Requesting temperatures...");
-  sensor1.requestTemperatures(); // Send the command to get temperatures
-  sensor2.requestTemperatures(); // Send the command to get temperatures  
-  sensor3.requestTemperatures(); // Send the command to get temperatures  
-  sensor4.requestTemperatures(); // Send the command to get temperatures 
-  sensor5.requestTemperatures(); // Send the command to get temperatures   
-  sensor6.requestTemperatures(); // Send the command to get temperatures 
+  //send the command to get temperatures
+  sensor1.requestTemperatures(); 
+  sensor2.requestTemperatures(); 
+  sensor3.requestTemperatures(); 
+  sensor4.requestTemperatures(); 
+  sensor5.requestTemperatures(); 
+  sensor6.requestTemperatures(); 
 
+  //retrieve temperature values and convert to degC
   T1 = sensor1.getTempCByIndex(0);
   T2 = sensor2.getTempCByIndex(0);
   T3 = sensor3.getTempCByIndex(0);
@@ -143,8 +110,9 @@ void getTempValues(void)
   T5 = sensor5.getTempCByIndex(0);
   T6 = sensor6.getTempCByIndex(0);
   
+  //debug
   Serial.println("DONE");
-  Serial.println(T1); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  Serial.println(T1); 
   Serial.println(T2); 
   Serial.println(T3); 
   Serial.println(T4); 
@@ -155,17 +123,18 @@ void getTempValues(void)
    //******************************************************
    // send package 1
    //******************************************************
+   //populate string with necessary data
    str0 = String("@T-Table_Exp") + "#" + nowStr +  "#" + String( T1 );
    str0 = str0 + String("@T-Room_Exp") + "#" + nowStr +  "#" + String( T2 );
    str0 = str0 + String("@T-RoomAC_Exp_AirOut") + "#" + nowStr +  "#" + String( T3 );
-   str0.toCharArray( UDPBuffer, str0.length()+1 );
-   Serial.println( UDPBuffer );
+   str0.toCharArray( UDPBuffer, str0.length()+1 );            //convert string to char array
+   Serial.println( UDPBuffer );                               //debug
    
-   Udp.beginPacket( outIP, outPort );
-   Udp.write(UDPBuffer);
-   Udp.endPacket();
+   Udp.beginPacket( outIP, outPort );                         //prepare for UDP transfer
+   Udp.write(UDPBuffer);                                      //transfer data
+   Udp.endPacket();                                           //end transfer
    
-   
+   //populate string with necessary data
    str0 = String("@T-TableAC_Exp_AirIn") + "#" + nowStr +  "#" + String( T4 );
    str0 = str0 + String("@T-TableAC_Exp_AirOut") + "#" + nowStr +  "#" + String( T5 );
    str0.toCharArray( UDPBuffer, str0.length()+1 );
@@ -187,8 +156,8 @@ void getTempValues(void)
 
 void loop(void)
 {
-   getTempValues();
-   delay( delayTime );
+   getTempValues();       //run temperature collection and UDP transfer
+   delay( delayTime );    //delay for 20 s
 }
 
 
